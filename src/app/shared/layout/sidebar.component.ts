ce1808d4364ef757843cb37eac92cdc8b4b02801
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-04-26 17:07:46 -0500

    Add SharedModule to use Font Awesome icons

--- src/app/shared/layout/sidebar.component.ts
+++ src/app/shared/layout/sidebar.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { SharedModule } from '@shared/shared.module';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faCoffee, faHome, faReceipt, faTags, faWallet, faCoins, faChartBar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [SharedModule],
   templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent {} 
+export class SidebarComponent {
+  faCoffee = faCoffee;
+  faHome = faHome;
+  faReceipt = faReceipt;
+  faTags = faTags;
+  faWallet = faWallet;
+  faCoins = faCoins;
+  faChartBar = faChartBar;
+
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faCoffee, faHome, faReceipt, faTags, faWallet, faCoins, faChartBar);
+  }
+}
