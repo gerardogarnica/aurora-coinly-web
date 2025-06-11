c9324acc2205f6d81937241e126f1e83d16744e3
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-06-11 15:09:44 -0500

    Add menu items to the sidebar

--- src/app/shared/layout/sidebar.component.ts
+++ src/app/shared/layout/sidebar.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, MenuModule],
   templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent{
+    sidebarMenu: MenuItem[] = [
+        {
+            label: 'Dashboard',
+            icon: 'pi pi-fw pi-chart-bar',
+            routerLink: ['/dashboard']
+        },
+        {
+            label: 'Transactions',
+            icon: 'pi pi-fw pi-receipt',
+            routerLink: ['/transactions']
+        },
+        {
+            label: 'Wallets',
+            icon: 'pi pi-fw pi-wallet',
+            routerLink: ['/wallets']
+        },
+        {
+            label: 'Categories',
+            icon: 'pi pi-fw pi-tags',
+            routerLink: ['/categories']
+        },
+        {
+            label: 'Payment Methods',
+            icon: 'pi pi-fw pi-credit-card',
+            routerLink: ['/methods']
+        },
+        {
+            label: 'Reports',
+            icon: 'pi pi-fw pi-table',
+            routerLink: ['/reports']
+        }
+    ];
 }
