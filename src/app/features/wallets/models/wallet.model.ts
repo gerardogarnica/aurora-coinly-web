b5f922b524fd85d11538c271acd2185d3e9cd1a8
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-06-24 21:45:10 -0500

    Add openedOn and lastOperationOn to wallet model

--- src/app/features/wallets/models/wallet.model.ts
+++ src/app/features/wallets/models/wallet.model.ts
@@ -14,6 +14,8 @@ export interface Wallet {
   canDelete: boolean;
   isDeleted: boolean;
   notes?: string;
+  openedOn: Date;
+  lastOperationOn: Date;
   transactions?: WalletTransactionModel[];
 }
 
