1e0ea95b504f2b2757c9aaca1caa0142d8626318
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-08-20 15:48:05 -0500

    Add token service

---
+++ src/app/core/services/token.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { getCookie, removeCookie, setCookie } from 'typescript-cookie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+  accessTokenKeyName: string = 'coinly_access_token';
+  refreshTokenKeyName: string = 'coinly_refresh_token';
+
+  getKeyName(type: TokenType): string {
+    return type === TokenType.Access ? this.accessTokenKeyName : this.refreshTokenKeyName;
+  }
+
+  getToken(type: TokenType): string | undefined {
+    const key = this.getKeyName(type);
+    return getCookie(key);
+  }
+
+  setToken(type: TokenType, token: string, expires: Date) {
+    const key = this.getKeyName(type);
+
+    setCookie(key, token, {
+      expires,
+      path: '/',
+      sameSite: 'strict'
+    });
+  }
+
+  removeToken(type: TokenType) {
+    const key = this.getKeyName(type);
+    removeCookie(key, { path: '/' });
+  }
+
+  hasToken(type: TokenType): boolean {
+    return !!this.getToken(type);
+  }
+}
+
+export enum TokenType {
+  Access = 'access',
+  Refresh = 'refresh'
+}
