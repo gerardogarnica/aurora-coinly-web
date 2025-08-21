3174048a16eb37dd72f05caad06dd550c951377e
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-08-20 20:52:10 -0500

    Add token handling and logout method to the auth service

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 import { ErrorsService } from '@core/services/errors.service';
+import { TokenService, TokenType } from '@core/services/token.service';
 import { IdentityToken } from '@features/auth/models/auth.model';
 
 @Injectable({
@@ -12,19 +13,36 @@ import { IdentityToken } from '@features/auth/models/auth.model';
 export class AuthService {
   private readonly httpClient = inject(HttpClient);
   private readonly errorsService = inject(ErrorsService);
+  private readonly tokenService = inject(TokenService);
   private readonly apiUrl = '/aurora/coinly/auth';
 
   login(email: string, password: string) {
     let url = `${this.apiUrl}/login`;
     return this.httpClient.post<IdentityToken>(url, { email, password }).pipe(
-      catchError(error => throwError(() => this.errorsService.handleHttpError(error)))
+      catchError(error => throwError(() => this.errorsService.handleHttpError(error))),
+      tap((token: IdentityToken) => {
+        // Store tokens in token service
+        this.tokenService.setToken(TokenType.Access, token.accessToken, token.accessTokenExpiresOn);
+        this.tokenService.setToken(TokenType.Refresh, token.refreshToken, token.refreshTokenExpiresOn);
+      })
     );
   }
 
   refreshToken(refreshToken: string) {
     let url = `${this.apiUrl}/refresh`;
     return this.httpClient.post<IdentityToken>(url, { refreshToken }).pipe(
-      catchError(error => throwError(() => this.errorsService.handleHttpError(error)))
+      catchError(error => throwError(() => this.errorsService.handleHttpError(error))),
+      tap((token: IdentityToken) => {
+        // Update tokens in token service
+        this.tokenService.setToken(TokenType.Access, token.accessToken, token.accessTokenExpiresOn);
+        this.tokenService.setToken(TokenType.Refresh, token.refreshToken, token.refreshTokenExpiresOn);
+      })
     );
+  }
+
+  logout() {
+    // Remove tokens from token service
+    this.tokenService.removeToken(TokenType.Access);
+    this.tokenService.removeToken(TokenType.Refresh);
   }
 }
