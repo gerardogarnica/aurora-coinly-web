1aea83e1a791288d5a02f4d2e2777f1320d66f9e
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-08-20 20:28:20 -0500

    Add call to refresh token service from private guard

--- src/app/core/guards/private.guard.ts
+++ src/app/core/guards/private.guard.ts
@@ -1,17 +1,39 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
+import { AuthService } from '@features/auth/services/auth.service';
 import { TokenService, TokenType } from '@core/services/token.service';
 
 export const privateGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
   const tokenService = inject(TokenService);
   const router = inject(Router);
 
-    const hasToken = tokenService.hasToken(TokenType.Access);
-    if (!hasToken) {
-    // If access token does not exist, redirect to login
-    router.navigate(['/auth']);
-    return false;
+  const hasAccessToken = tokenService.hasToken(TokenType.Access);
+  if (!hasAccessToken) {
+    const hasRefreshToken = tokenService.hasToken(TokenType.Refresh);
+    if (!hasRefreshToken) {
+      router.navigate(['/auth']);
+      return false;
+    }
+
+    // Call refresh token endpoint
+    const refreshToken = tokenService.getToken(TokenType.Refresh);
+    if (!refreshToken) {
+      router.navigate(['/auth']);
+      return false;
+    }
+
+    authService.refreshToken(refreshToken).subscribe({
+      next: () => {
+        // Successfully refreshed token, allow access
+      },
+      error: () => {
+        // Refresh failed, redirect to login
+        router.navigate(['/auth']);
+        return false;
+      }
+    });
   }
 
   // Allow access to the route
--- src/app/core/guards/public.guard.ts
+++ src/app/core/guards/public.guard.ts
@@ -7,8 +7,8 @@ export const publicGuard: CanActivateFn = (route, state) => {
   const tokenService = inject(TokenService);
   const router = inject(Router);
 
-  const hasToken = tokenService.hasToken(TokenType.Access);
-  if (hasToken) {
+  const hasAccessToken = tokenService.hasToken(TokenType.Access);
+  if (hasAccessToken) {
     // If access token exists, redirect to home
     router.navigate(['/']);
     return false;
