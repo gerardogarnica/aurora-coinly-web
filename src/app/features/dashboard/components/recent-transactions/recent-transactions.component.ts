7ad8933612c2e37ee30ca2c6903b51599818289c
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-09-12 11:56:54 -0500

    Refactor recent and upcoming transactions components for improved layout and status display

--- src/app/features/dashboard/components/recent-transactions/recent-transactions.component.ts
+++ src/app/features/dashboard/components/recent-transactions/recent-transactions.component.ts
@@ -2,14 +2,15 @@ import { Component, Input } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 
 import { Transaction } from '@features/transactions/models/transaction.model';
-import { TransactionType } from '@features/transactions/models/transaction.types';
+import { TransactionStatus, TransactionType } from '@features/transactions/models/transaction.types';
 import { CommonUtils } from '@shared/utils/common.utils';
 
 import { AvatarModule } from 'primeng/avatar';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-recent-transactions',
-  imports: [SharedModule, AvatarModule],
+  imports: [SharedModule, AvatarModule, TagModule],
   templateUrl: './recent-transactions.component.html'
 })
 export class RecentTransactionsComponent {
@@ -29,5 +30,9 @@ export class RecentTransactionsComponent {
 
   getTransactionAmountClass(transaction: Transaction): string {
     return transaction.type == TransactionType.Expense ? 'text-coinly-danger' : 'text-coinly-success';
+  }
+
+  getTransactionStatusSeverity(status: TransactionStatus): string {
+    return status === TransactionStatus.Pending ? 'warn' : 'success';
   }
 }
