a0b271f972b91a2100a59b263dbf9809c4a14c08
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-09-30 20:59:07 -0500

    Update icon for New Transaction button in navbar and adjust severity

--- src/app/features/transactions/components/new-transaction-form/new-transaction-form.component.ts
+++ src/app/features/transactions/components/new-transaction-form/new-transaction-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, inject, Input, Output, signal } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
